7a499ef9e7508f8fcb9aae8d3ffd0b20e2fc3a42
GoldenPuffz <vphan004@gmail.com>
2024-09-23 10:57:31 -0400

    upload

---
+++ tutorial.c
@@ -0,0 +1,81 @@
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+/*void squareNum(int number){
+    //if function doesn't return anything, use void
+    int square = number * number;
+    printf("%d", square);
+}
+*/
+int main(){
+    
+    int age1, age2 = 25;
+
+    printf("c-programming\n");
+    //printf("Age: %d", age1);
+
+    //double number = 12.45;
+    //printf("%f.2, number"); //only 2 decimals points are printed
+
+    //float number1 = 10.9f;
+    //printf("\n%.1f, number1");
+
+    //int uv;
+    //scanf("%d", uv);//&uv id address of uv variable 
+
+    //int num;
+    //char word;
+    //scanf("%d %c", &num, &word);
+
+
+/*
+    double a = 5.67;
+    int b = 9;
+    double result = (int)a + b;
+    printf("%lf", result);
+
+    bool value = (5 < 9); //this will equal 0 for false 1 for true
+
+    int age = 15;
+    if(age > 17){
+        printf("You can vote");
+        }
+    else{ 
+        printf("You can't vote");
+        }
+
+*/
+return 0;
+    
+}
+/*notes
+4 bytes = 32bits
+2^32 distinct values an invariablee can store 
+int %d
+double %lf
+float %f
+char %c
+bool
+
+&& || !
++
+-
+*
+/
+%
+++
+--
+TERNARY OPERATORS
+test_condition ? expression1 : expression2;
+if true 1st expresses if false 2nd
+DATA TYPE HIERARCHY
+long double
+double
+float
+long
+int
+short
+char
+*/
